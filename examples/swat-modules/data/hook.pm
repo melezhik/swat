cedf6c1746ca63242937301d8e1dee480e348530
Vagrant User <vagrant@Debian-jessie-amd64-netboot.vagrantup.com>
2015-10-08 18:43:28 +0300

    fix for test case

--- examples/swat-modules/data/hook.pm
+++ examples/swat-modules/data/hook.pm
@@ -1,6 +1,10 @@
 run_swat_module( 'GET' => '/wife' , { path => '/data.txt' } );
 run_swat_module( 'GET' => '/son', { path => '/data.txt' });
 
+modify_resource( sub {
+    '/data.txt'
+});
+
 1;
 
 
