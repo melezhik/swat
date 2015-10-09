fe06b873ff5d81cf117e53c3d097ce26789b2aeb
Vagrant User <vagrant@Debian-jessie-amd64-netboot.vagrantup.com>
2015-10-09 11:16:04 +0300

    update pod

--- lib/swat.pm
+++ lib/swat.pm
@@ -370,3 +370,22 @@ sub handle_plain {
 1;
 
 
+__END__
+
+=head1 SYNOPSIS
+
+Web automated testing framework.
+
+=head1 Documentation
+
+Please follow github pages  - https://github.com/melezhik/swat
+
+=head1 AUTHOR
+
+Aleksei Melezhik
+
+=head1 COPYRIGHT
+
+Copyright 2015 Alexey Melezhik.
+
+This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
